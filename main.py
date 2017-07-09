15cc626c0660d7928ca44e3088ec9fb3e50e7e46
cassiandrei <cschneider@inf.ufsm.br>
2017-07-09 12:06:25 -0300

    inicio e fianis

--- main.py
+++ main.py
@@ -11,18 +11,29 @@ class Automato(AutomatonGrammarListener):
     alfabeto = []
     estados = []
     transicoes = []
+    inicio = ""
+    final = []
 
     def enterAlf(self, ctx:AutomatonGrammarParser.AlfContext):
         self.alfabeto.append(ctx.letra().getText())
         pass
 
     def enterEstados(self, ctx:AutomatonGrammarParser.EstadosContext):
-        self.estados.append(ctx.estado().getText())
+        self.estados = ctx.estado().getText().split(",")
         pass
 
     def enterTrans(self, ctx:AutomatonGrammarParser.TransContext):
         self.transicoes.append(ctx.getText())
         pass
+
+    def enterInicio(self, ctx:AutomatonGrammarParser.InicioContext):
+        self.inicio = ctx.est().getText()
+        pass
+
+    def enterFinal(self, ctx:AutomatonGrammarParser.FinalContext):
+        self.final = ctx.estado().getText().split(",")
+        pass
+
 
     def visitErrorNode(self, node):
         print("Erro na entrada!!!!")
@@ -55,12 +66,17 @@ def main(argv):
 
     entrada.transicoes = entrada.transicoes[0].replace(",(", ";(").split(";")
     for cont in range(len(entrada.transicoes)):
-        entrada.transicoes[cont] = entrada.transicoes[cont].replace("(","")
+        entrada.transicoes[cont] = entrada.transicoes[cont].replace("(", "")
         entrada.transicoes[cont] = entrada.transicoes[cont].replace(")", "")
         entrada.transicoes[cont] = entrada.transicoes[cont].replace("=", ",")
         entrada.transicoes[cont] = entrada.transicoes[cont].split(",")
     print("Transicoes: ")
     print(entrada.transicoes)
 
+    print("Inicio: ")
+    print(entrada.inicio)
+
+    print("Finais: ")
+    print(entrada.final)
 if __name__ == '__main__':
     main(sys.argv)
